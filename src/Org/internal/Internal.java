64e2bb70a64ef7363003b98509f12178387075be
unknown <mohamedsheik16>
2021-02-26 15:03:38 +0530

    New project

---
+++ src/Org/internal/Internal.java
@@ -0,0 +1,28 @@
+package Org.internal;
+
+import ora.mobile.ExternalStoreage;
+
+public class Internal {
+	
+	private void Internal() {
+		// TODO Auto-generated method stub
+	 
+		System.out.println("Phone internal storeage is 16 GB");
+	}	
+
+	  public static void main(String[] args) {
+		
+
+		  Internal In =new Internal();
+		  ExternalStoreage ex =new ExternalStoreage();
+		  
+		  
+		  
+
+		  In.Internal();
+		  ex.External();
+
+	}
+	  
+		
+}
---
+++ src/ora/mobile/ExternalStoreage.java
@@ -0,0 +1,22 @@
+package ora.mobile;
+
+public class ExternalStoreage {
+	
+public void External() {
+	// TODO Auto-generated method stub
+ 
+	System.out.println("Phone external storeage is 8 GB");
+}	
+
+  public static void main(String[] args) {
+	  
+	  ExternalStoreage Ex =new ExternalStoreage();
+
+	  Ex.External();
+	
+}
+	
+	 
+	  
+}
+
